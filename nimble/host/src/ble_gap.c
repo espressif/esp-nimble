ae47305152f265a18ac870438ca798376feaf350
Rahul Tank <rahul.tank@espressif.com>
2021-12-07 20:31:04 +0530

    H2 chip support

--- nimble/host/src/ble_gap.c
+++ nimble/host/src/ble_gap.c
@@ -127,7 +127,9 @@ struct ble_gap_connect_reattempt_ctxt {
 };
 
 static struct ble_gap_connect_reattempt_ctxt ble_conn_reattempt[MYNEWT_VAL(BLE_MAX_CONNECTIONS)];
+#if !MYNEWT_VAL(BLE_EXT_ADV)
 static uint16_t reattempt_idx;
+#endif
 static bool conn_cookie_enabled;
 #endif
 
@@ -4459,7 +4461,7 @@ ble_gap_periodic_adv_stop(uint8_t instance)
     return rc;
 }
 
-static void
+void
 ble_gap_npl_sync_lost(struct ble_npl_event *ev)
 {
     struct ble_hs_periodic_sync *psync;
