8572a999dd614ee9f2d9a5be37c313de34aa5c7f
Donatien Garnier <donatien.garnier@blecon.net>
2023-12-21 11:50:00 +0000

    nimble/host: Read supported HCI commands from controller
    This patch adds a new function, ble_hs_startup_read_sup_cmd_tx(), which
    reads the list of supported HCI commands from the controller on HCI
    startup. This is used to determine whether a HCI Set Event
    Mask Page 2 command should be sent to the controller.

--- nimble/host/src/ble_hs_hci_priv.h
+++ nimble/host/src/ble_hs_hci_priv.h
@@ -80,7 +80,7 @@ struct hci_periodic_adv_params
 #endif
 
 struct ble_hs_hci_sup_cmd {
-    uint8_t commands[64];
+    unsigned event_mask2 : 1; /** Indicates whether the controller supports the set event mask page 2 command */
 };
 
 extern uint16_t ble_hs_hci_avail_pkts;
