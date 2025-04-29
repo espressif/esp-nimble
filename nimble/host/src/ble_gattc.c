aea7ef1217d1e83add5170a30bf0903b7417a139
Deomid rojer Ryabkov <rojer@rojer.me>
2025-04-30 00:58:51 +0300

    ble_gattc: Fix proc rx entry comparison
    Incorrect comparison causes request/response mismatches when multiple ops are in flight
    
    Bug introduced in https://github.com/espressif/esp-nimble/commit/a690a0596597294fd85bee397e9df9ca81b0b48d

--- nimble/host/src/ble_gattc.c
+++ nimble/host/src/ble_gattc.c
@@ -989,8 +989,8 @@ ble_gattc_proc_matches_conn_rx_entry(struct ble_gattc_proc *proc, void *arg)
     criteria = arg;
 
     if (criteria->conn_handle != BLE_HS_CONN_HANDLE_NONE &&
-        criteria->conn_handle != proc->conn_handle &&
-        criteria->cid != proc->cid) {
+        (criteria->conn_handle != proc->conn_handle ||
+         criteria->cid != proc->cid)) {
 
         return 0;
     }
