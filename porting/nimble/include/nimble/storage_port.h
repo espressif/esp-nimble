450d35aae01161d271c6eb2cddc6444519baee67
Astha Verma <astha.verma@espressif.com>
2025-09-26 18:53:11 +0530

    fix(nimble): Modify GATT data storage struture in NVS during gatt caching

--- porting/nimble/include/nimble/storage_port.h
+++ porting/nimble/include/nimble/storage_port.h
@@ -31,15 +31,19 @@ typedef uint32_t cache_handle_t;
 typedef int (*open_cache)(const char *namespace_name, open_mode_t open_mode, cache_handle_t *out_handle);
 typedef void (*close_cache)(cache_handle_t handle);
 typedef int (*erase_all_cache)(cache_handle_t handle);
+typedef int (*erase_key) (cache_handle_t handle, const char *key);
 typedef int (*write_cache)(cache_handle_t handle, const char *key, const void* value, size_t length);
 typedef int (*read_cache)(cache_handle_t handle, const char *key, void* out_value, size_t *length);
+typedef int (*commit_cache)(cache_handle_t handle);
 
 struct cache_fn_mapping {
     open_cache open;
     close_cache close;
+    erase_key erase_key_item;
     erase_all_cache erase_all;
     write_cache write;
     read_cache read;
+    commit_cache commit;
 };
 
 struct cache_fn_mapping link_storage_fn(void *storage_cb);
