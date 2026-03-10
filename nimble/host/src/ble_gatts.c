edb63b274e1639eaf86d1ae69b99957e84063ad7
Astha Verma <astha.verma@espressif.com>
2026-03-10 12:48:29 +0530

    fix(nimble): fix null dereference of ble_gatts_conn_aware_states after gatts stop

--- nimble/host/src/ble_gatts.c
+++ nimble/host/src/ble_gatts.c
@@ -1666,6 +1666,9 @@ ble_gatts_connection_broken(uint16_t conn_handle)
         /* update bonded peer aware state */
         if(conn->bhc_sec_state.bonded) {
             ble_hs_conn_addrs(conn, &addrs);
+#if MYNEWT_VAL(BLE_STATIC_TO_DYNAMIC)
+            if (ble_gatts_conn_aware_states != NULL) {
+#endif
             for(i = 0; i < MYNEWT_VAL(BLE_STORE_MAX_BONDS); i++) {
                 if(memcmp(ble_gatts_conn_aware_states[i].peer_id_addr,
                           addrs.peer_id_addr.val, sizeof addrs.peer_id_addr.val) == 0) { //Check Thoroughly
@@ -1678,6 +1681,9 @@ ble_gatts_connection_broken(uint16_t conn_handle)
                     }
                 }
             }
+#if MYNEWT_VAL(BLE_STATIC_TO_DYNAMIC)
+        }
+#endif
         }
 #endif
     }
@@ -2770,6 +2776,9 @@ ble_gatts_bonding_restored(uint16_t conn_handle)
 #if MYNEWT_VAL(BLE_GATT_CACHING)
     /* update the aware state of the client */
     ble_hs_conn_addrs(conn, &addrs);
+#if MYNEWT_VAL(BLE_STATIC_TO_DYNAMIC)
+    if (ble_gatts_conn_aware_states != NULL) {
+#endif
     for(i = 0; i < MYNEWT_VAL(BLE_STORE_MAX_BONDS); i++) {
         if(memcmp(ble_gatts_conn_aware_states[i].peer_id_addr,
                           addrs.peer_id_addr.val, sizeof addrs.peer_id_addr.val) == 0) { // Check Thoroughly
@@ -2777,6 +2786,9 @@ ble_gatts_bonding_restored(uint16_t conn_handle)
             conn->bhc_gatt_svr.aware_state = ble_gatts_conn_aware_states[i].aware;
         }
     }
+#if MYNEWT_VAL(BLE_STATIC_TO_DYNAMIC)
+    }
+#endif
 #endif
     ble_hs_unlock();
 
@@ -3194,11 +3206,17 @@ int ble_gatts_add_dynamic_svcs(const struct ble_gatt_svc_def *svcs) {
     end_handle = entry->end_group_handle;
 #if MYNEWT_VAL(BLE_GATT_CACHING)
     /* make all bonded connections unaware */
+#if MYNEWT_VAL(BLE_STATIC_TO_DYNAMIC)
+    if (ble_gatts_conn_aware_states != NULL) {
+#endif
     for(i = 0; i < MYNEWT_VAL(BLE_STORE_MAX_BONDS); i++) {
         ble_gatts_conn_aware_states[i].half_aware = 0;
         ble_gatts_conn_aware_states[i].aware = false;
     }
     ble_hs_conn_foreach(ble_gatts_conn_unaware, NULL);
+#if MYNEWT_VAL(BLE_STATIC_TO_DYNAMIC)
+    }
+#endif
 #endif
 
     /* send service change indication */
@@ -3295,11 +3313,17 @@ done:
         rc = ble_gatts_remove_svc_entry(uuid);
 #if MYNEWT_VAL(BLE_GATT_CACHING)
         /* make all bonded connections them unaware */
+#if MYNEWT_VAL(BLE_STATIC_TO_DYNAMIC)
+        if (ble_gatts_conn_aware_states != NULL) {
+#endif
         for(i = 0; i < MYNEWT_VAL(BLE_STORE_MAX_BONDS); i++) {
             ble_gatts_conn_aware_states[i].aware = false;
             ble_gatts_conn_aware_states[i].half_aware = 0;
         }
         ble_hs_conn_foreach(ble_gatts_conn_unaware, NULL);
+#if MYNEWT_VAL(BLE_STATIC_TO_DYNAMIC)
+        }
+#endif
 #endif
 
         /* send service change indication */
