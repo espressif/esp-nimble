095d93bd11cd48a88d64b8729f6039a487a3600d
ShenWeilong <shenweilong@espressif.com>
2026-03-27 19:56:34 +0800

    feat(nimble): Added npl for btdm sofware arch

--- nimble/host/src/ble_hs.c
+++ nimble/host/src/ble_hs.c
@@ -715,7 +715,7 @@ ble_hs_enqueue_hci_event(uint8_t *hci_evt)
 
     ev = os_memblock_get(&ble_hs_hci_ev_pool);
 
-    if (ev && ble_hs_evq->eventq) {
+    if (ev) {
         memset (ev, 0, sizeof *ev);
         ble_npl_event_init(ev, ble_hs_event_rx_hci_ev, hci_evt);
         ble_npl_eventq_put(ble_hs_evq, ev);
--- porting/nimble/include/os/endian.h
+++ porting/nimble/include/os/endian.h
@@ -207,6 +207,117 @@ extern "C" {
 #endif
 
 #if SOC_ESP_NIMBLE_CONTROLLER && CONFIG_BT_CONTROLLER_ENABLED
+#if CONFIG_BT_DUAL_MODE_ARCH
+#include "btdm_endian.h"
+
+static inline void
+put_le16(void *buf, uint16_t x)
+{
+    btdm_put_le16(buf, x);
+}
+
+static inline void
+put_le24(void *buf, uint32_t x)
+{
+    btdm_put_le24(buf, x);
+}
+
+static inline void
+put_le32(void *buf, uint32_t x)
+{
+    btdm_put_le32(buf, x);
+}
+
+static inline void
+put_le64(void *buf, uint64_t x)
+{
+    btdm_put_le64(buf, x);
+}
+
+static inline uint16_t
+get_le16(const void *buf)
+{
+    return btdm_get_le16(buf);
+}
+
+static inline uint32_t
+get_le24(const void *buf)
+{
+    return btdm_get_le24(buf);
+}
+
+static inline uint32_t
+get_le32(const void *buf)
+{
+    return btdm_get_le32(buf);
+}
+
+static inline uint64_t
+get_le64(const void *buf)
+{
+    return btdm_get_le64(buf);
+}
+
+static inline void
+put_be16(void *buf, uint16_t x)
+{
+    btdm_put_be16(buf, x);
+}
+
+static inline void
+put_be24(void *buf, uint32_t x)
+{
+    btdm_put_be24(buf, x);
+}
+
+static inline void
+put_be32(void *buf, uint32_t x)
+{
+    btdm_put_be32(buf, x);
+}
+
+static inline void
+put_be64(void *buf, uint64_t x)
+{
+    btdm_put_be64(buf, x);
+}
+
+static inline uint16_t
+get_be16(const void *buf)
+{
+    return btdm_get_be16(buf);
+}
+
+static inline uint32_t
+get_be24(const void *buf)
+{
+    return btdm_get_be24(buf);
+}
+
+static inline uint32_t
+get_be32(const void *buf)
+{
+    return btdm_get_be32(buf);
+}
+
+static inline uint64_t
+get_be64(const void *buf)
+{
+    return btdm_get_be64(buf);
+}
+
+static inline void
+swap_in_place(void *buf, int len)
+{
+    btdm_swap_in_place(buf, len);
+}
+
+static inline void
+swap_buf(uint8_t *dst, const uint8_t *src, int len)
+{
+    btdm_swap_buf(dst, src, len);
+}
+#else // CONFIG_BT_DUAL_MODE_ARCH
 void r_put_le16(void *buf, uint16_t x);
 #define put_le16 r_put_le16
 
@@ -260,7 +371,7 @@ void r_swap_in_place(void *buf, int len);
 
 void r_swap_buf(uint8_t *dst, const uint8_t *src, int len);
 #define swap_buf r_swap_buf
-
+#endif // CONFIG_BT_DUAL_MODE_ARCH
 
 #else
 void put_le16(void *buf, uint16_t x);
